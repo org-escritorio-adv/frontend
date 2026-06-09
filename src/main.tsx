e15ae8eaab347f9316b2b6f40cffc5c89e201489
juliatakaki <julia.takaki@gmail.com>
2026-06-09 16:29:32 -0300

    feat: adiciona configuracao do keycloak e ajusta inicializacao no main

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,19 @@
-import { createRoot } from "react-dom/client";
-import App from "./app/App.tsx";
-import "./styles/index.css";
-import { initKeycloak } from "./lib/keycloak";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './app/App.tsx';
+import keycloak from './keycloak';
+import './styles/index.css';
 
-initKeycloak().then(() => {
-  createRoot(document.getElementById("root")!).render(<App />);
+keycloak.init({ onLoad: 'login-required' }).then((authenticated) => {
+  if (authenticated) {
+    localStorage.setItem('token', keycloak.token || '');
+    
+    ReactDOM.createRoot(document.getElementById('root')!).render(
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    );
+  }
+}).catch((err) => {
+  console.error("Erro ao autenticar no Keycloak", err);
 });
